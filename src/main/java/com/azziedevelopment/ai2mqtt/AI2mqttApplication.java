2e6f6aba638c5ea8a2d174e7375a92b6445b6c71
AzzieDev <18339356+AzzieDev@users.noreply.github.com>
2025-12-08 04:10:05 -0500

    initial spring starter

---
+++ src/main/java/com/azziedevelopment/ai2mqtt/AI2mqttApplication.java
@@ -0,0 +1,13 @@
+package com.azziedevelopment.ai2mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AI2mqttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AI2mqttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azziedevelopment/ai2mqtt/AI2MqttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azziedevelopment.ai2mqtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AI2MqttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
