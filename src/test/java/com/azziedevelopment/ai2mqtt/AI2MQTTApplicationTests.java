48647a8e754b51e617b755ac8a7bc7e01553df6a
AzzieDev <18339356+AzzieDev@users.noreply.github.com>
2025-12-08 06:52:08 -0500

    Add dummy values to tests

--- src/test/java/com/azziedevelopment/ai2mqtt/AI2MQTTApplicationTests.java
+++ src/test/java/com/azziedevelopment/ai2mqtt/AI2MQTTApplicationTests.java
@@ -3,7 +3,15 @@ package com.azziedevelopment.ai2mqtt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	// Provide dummy values so @Value fields don't crash the startup
+	"openai.api.key=dummy-test-key",
+	"mqtt.username=test-user",
+	"mqtt.password=test-pass",
+	"mqtt.broker.url=tcp://localhost:1883",
+	"spring.datasource.url=jdbc:h2:mem:testdb", // Use in-memory DB for tests
+	"messaging.type=mqtt" // Force a specific mode to test
+})
 class AI2MQTTApplicationTests {
 
 	@Test
