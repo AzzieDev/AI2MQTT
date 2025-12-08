1deda7651a9405857bbda62f6fc419178289ec60
AzzieDev <18339356+AzzieDev@users.noreply.github.com>
2025-12-08 06:06:33 -0500

    rename

--- src/test/java/com/azziedevelopment/ai2mqtt/AI2MqttApplicationTests.java
+++ src/test/java/com/azziedevelopment/ai2mqtt/AI2MqttApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AI2MqttApplicationTests {
+class AI2MQTTApplicationTests {
 
 	@Test
 	void contextLoads() {
