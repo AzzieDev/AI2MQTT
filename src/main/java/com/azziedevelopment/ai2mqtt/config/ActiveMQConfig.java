8346dcf06c15c6997aaf7c0d47b851701c07bac1
AzzieDev <18339356+AzzieDev@users.noreply.github.com>
2025-12-09 06:12:05 -0500

    Add qualifier

--- src/main/java/com/azziedevelopment/ai2mqtt/config/ActiveMQConfig.java
+++ src/main/java/com/azziedevelopment/ai2mqtt/config/ActiveMQConfig.java
@@ -1,14 +1,13 @@
 package com.azziedevelopment.ai2mqtt.config;
 
 import jakarta.jms.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
+
 
 /**
  * Ensures the necessary infrastructure beans for ActiveMQ are present
@@ -18,20 +17,21 @@ import org.springframework.jms.support.converter.MessageType;
 @ConditionalOnProperty(name = "messaging.type", havingValue = "activemq")
 public class ActiveMQConfig {
 
-    /**
-     * Defines the required JmsListenerContainerFactory for the @JmsListener annotation.
-     */
-    @Bean
-    public JmsListenerContainerFactory<?> jmsListenerContainerFactory(ConnectionFactory connectionFactory) {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        
-        // This is important for processing topic messages concurrently
-        factory.setPubSubDomain(true); 
-        
-        // Optional: Ensure messages are processed in parallel (better throughput)
-        factory.setConcurrency("3-10"); 
-        
-        return factory;
-    }
+	/**
+	 * Defines the required JmsListenerContainerFactory for the @JmsListener annotation.
+	 */
+	@Bean
+	public JmsListenerContainerFactory<?> jmsListenerContainerFactory(
+		@Qualifier("jmsConnectionFactory") ConnectionFactory connectionFactory) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+
+		// This is important for processing topic messages concurrently
+		factory.setPubSubDomain(true);
+
+		// Optional: Ensure messages are processed in parallel (better throughput)
+		factory.setConcurrency("3-10");
+
+		return factory;
+	}
 }
